f6d12a3ac8ff2aaaf294df16ff1aa5b68cbb6d05
Mateus Simao <mateus.david@outlook.com>
2019-09-14 15:41:13 -0300

    retirando conexão com o banco

--- aplicacao/app.py
+++ aplicacao/app.py
@@ -1,13 +1,10 @@
 from flask import Flask, jsonify, request, render_template
-from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'matherick'
-app.config['MYSQL_DB'] = 'not_iridium'
-
+database = dict()
+database['USUARIO'] = {}
+database['PACIENTE'] = {}
 
 @app.route("/")
 def index(): 
