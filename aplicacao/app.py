1fec5a78882fa25097d335d63fabcf107fbbaae5
Mateus Simao <mateus.david@outlook.com>
2019-09-14 17:03:20 -0300

    Adicionando as rotas de login e paciente

--- aplicacao/app.py
+++ aplicacao/app.py
@@ -13,7 +13,11 @@ def index():
 
 @app.route("/login", methods=['POST'])
 def fazer_login():
-    return 'Hello World usuario'
+    login = request.form
+    for usuario in database['USUARIO']:
+        if login['nusp'] == usuario['nusp'] and login['senha'] == usuario['senha']:
+            return 'Usuario encontrado', 200
+    return {'erro': 'Usuario não encontrado'}, 404
 
 
 @app.route("/login/cadastro", methods=['POST'])
@@ -25,6 +29,52 @@ def fazer_cadastro_usuario():
     else:
         return 'Email não informado', 404
     return jsonify(dados_usuario)
+
+
+@app.route("/paciente")
+def listar_paciente():
+    return jsonify(database['PACIENTE'])
+
+
+@app.route("/paciente", methods=['POST'])
+def fazer_cadastro_paciente():
+    dados_paciente = request.values
+
+    if 'nome' not in dados_paciente:
+        return {'erro': 'Nome não informado'}, 400
+
+    if len(dados_paciente['nome']) < 6:
+        return {'erro': 'Informe o nome completo'}, 400
+
+    if 'sexo' not in dados_paciente:
+        return {'erro': 'Sexo não informado'}, 400
+
+    if dados_paciente['sexo'].upper() not in ['M', 'F', 'O']:
+        return {'erro': 'Sexo invalido'}, 400
+
+    if 'data_nascimento' not in dados_paciente:
+        return {'erro': 'Data de nascimento não informado'}, 400
+
+    if 'cpf' not in dados_paciente:
+        return {'erro': 'CPF não informado'}, 400
+
+    if len(dados_paciente['cpf']) < 11:
+        return {'erro': 'CPF invalido'}, 400
+
+    # Verificando se o paciente já existe
+    for paciente in database['PACIENTE']:
+        if paciente['cpf'] == dados_paciente['cpf']:
+            return {'erro': 'CPF já existente no cadastro'}, 400
+
+    novo_paciente = {
+        'nome': dados_paciente['nome'],
+        'sexo': dados_paciente['sexo'].upper(),
+        'data_nascimento': dados_paciente['data_nascimento'],
+        'cpf': dados_paciente['cpf']
+    }
+    database['PACIENTE'].append(novo_paciente)
+
+    return 'Paciente cadastrado com sucesso', 201
 
 
 if __name__ == "__main__":
