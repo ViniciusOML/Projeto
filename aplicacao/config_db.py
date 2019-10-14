ff8915fb263768808cd0c7a937187c7815db2766
Deivid Valença de Araujo <deivid.araujo@aluno.faculdadeimpacta.com.br>
2019-10-13 23:49:01 -0300

    Persistindo usuarios no banco, add campo nusp

--- aplicacao/config_db.py
+++ aplicacao/config_db.py
@@ -3,8 +3,9 @@ from flask_mysqldb import MySQL
 
 def configuraConexao(app):
     app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'matherick'
+    app.config['MYSQL_USER'] = 'deivid'
+    app.config['MYSQL_PASSWORD'] = 'deivid'
     app.config['MYSQL_DB'] = 'ope'
+    app.config['MYSQL CURSORCLASS'] = 'DictCursor'
 
     return MySQL(app)
