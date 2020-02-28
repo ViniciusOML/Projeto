4a1b364998ba0bc194d37c6e9394a9ecd72a505f
Mateus Simao <mateus.david@outlook.com>
2020-02-28 13:21:34 -0300

    Adicionando tabelas de atendimentos, lifs, procedimentos, atendimento_procedimentos, atendimento_procedimento_beras

--- aplicacao/banco.sql
+++ aplicacao/banco.sql
@@ -21,3 +21,52 @@ CREATE TABLE IF NOT EXISTS usuarios (
     createdAt DATETIME DEFAULT NOW(),
     updatedAt DATETIME DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS atendimentos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_paciente INT NOT NULL,
+    id_lif INT NOT NULL,
+    data_consulta DATE,
+);
+
+CREATE TABLE IF NOT EXISTS lifs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(80),
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW()
+);
+
+CREATE TABLE procedimentos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    sigla VARCHAR(10),
+    procedimento CHAR(1) NOT NULL,
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW()
+);
+
+-- 1 BERA
+-- 2
+-- 3
+
+CREATE TABLE IF NOT EXISTS atendimento_procedimentos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_atendimento INT NOT NULL,
+    id_procedimento INT NOT NULL,
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS atendimento_procedimento_beras (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_atendimento_procedimento INT PRIMARY KEY,
+    evolucao TEXT,
+    conclusao TEXT,
+    createdAt DATETIME DEFAULT NOW(),
+    updatedAt DATETIME DEFAULT NOW()
+);
+
+-- CREATE TABLE IF NOT EXISTS resultado_beras (
+--     id  INT PRIMARY KEY AUTO_INCREMENT,
+--     id_atendimento_procedimento_bera INT NOT NULL,
+--     direto
+-- );
