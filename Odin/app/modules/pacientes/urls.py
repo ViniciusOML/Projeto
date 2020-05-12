aa74c595c5e7601594e7d8fef3e35bb4832629d2
Mateus Simão <mateus.david@outlook.com>
2020-05-12 16:12:49 -0300

    arrumando redirecionamentos e usando o DetailView

--- Odin/app/modules/pacientes/urls.py
+++ Odin/app/modules/pacientes/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from .views import PacienteListView, PacienteCreateView
-from .views import PacienteUpdateView, PacienteDeleteView
-from .views import PacienteAtendimentoCreateView
+from .views import PacienteListView,\
+    PacienteCreateView,\
+    PacienteUpdateView,\
+    PacienteDeleteView,\
+    PacienteAtendimentoCreateView,\
+    PacienteDetailView
 
 
 urlpatterns = [
     path('', PacienteListView.as_view(), name='index_pacientes'),
     path('new', PacienteCreateView.as_view(), name='new_paciente'),
+    path('show/<int:pk>/', PacienteDetailView.as_view(), name='show_paciente'),
     path('update/<int:pk>/', PacienteUpdateView.as_view(), name='update_paciente'),
     path('delete/<int:pk>/', PacienteDeleteView.as_view(), name='delete_paciente'),
     path('<int:pk>/atendimento/new', PacienteAtendimentoCreateView.as_view(), name='new_paciente_atendimento')
--- Odin/app/modules/pacientes/views.py
+++ Odin/app/modules/pacientes/views.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from ...models import Paciente, Atendimento, Consulta
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
 
 class PacienteListView(LoginRequiredMixin, ListView):
@@ -13,45 +13,12 @@ class PacienteListView(LoginRequiredMixin, ListView):
     context_object_name = 'pacientes'
 
 
-class PacienteCreateView(LoginRequiredMixin, CreateView):
+class PacienteDetailView(LoginRequiredMixin, DetailView):
     login_url = '/'
 
     model = Paciente
-    fields = [
-        'nome_completo', 'cpf', 'rg', 'data_nascimento',
-        'sexo', 'tem_responsavel', 'nome_responsavel', 'rg_responsavel'
-    ]
-    template_name = 'paciente/novo.html'
-    success_url = reverse_lazy('index_pacientes')
-
-
-class PacienteAtendimentoCreateView(LoginRequiredMixin, CreateView):
-    login_url = '/'
-
-    model = Atendimento
-    fields = ['codigo_lif', 'lif']
-    template_name = 'paciente/atendimento_novo.html'
-    success_url = reverse_lazy('index_atendimentos')
-
-    def form_valid(self, form):
-        form.instance.paciente = Paciente.objects.get(id=self.kwargs['pk'])
-        form.save()
-        return super(PacienteAtendimentoCreateView, self).form_valid(form)
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['paciente'] = Paciente.objects.get(id=self.kwargs['pk'])
-        return context
-
-
-class PacienteUpdateView(LoginRequiredMixin, UpdateView):
-    login_url = '/'
-
-    model = Paciente
-    fields = ['nome_completo', 'cpf', 'rg', 'data_nascimento',
-              'sexo', 'tem_responsavel', 'nome_responsavel', 'rg_responsavel']
-    template_name = 'paciente/editar.html'
-    success_url = reverse_lazy('index_pacientes')
+    template_name = 'paciente/show.html'
+    success_url = reverse_lazy('show_pacientes')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -63,7 +30,7 @@ class PacienteUpdateView(LoginRequiredMixin, UpdateView):
         context['consultas'] = []
 
         for atendimento in atendimentos:
-            context['atendimentos'].append(atendimento)            
+            context['atendimentos'].append(atendimento)
 
             # Obtendo as consultas do atendimento
             consultas = Consulta.objects.filter(atendimento_id=atendimento.id)
@@ -71,6 +38,53 @@ class PacienteUpdateView(LoginRequiredMixin, UpdateView):
             for consulta in consultas:
                 context['consultas'].append(consulta)
         return context
+
+
+class PacienteCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/'
+
+    model = Paciente
+    fields = [
+        'nome_completo', 'cpf', 'rg', 'data_nascimento',
+        'sexo', 'tem_responsavel', 'nome_responsavel', 'rg_responsavel'
+    ]
+    template_name = 'paciente/novo.html'
+
+    def get_success_url(self):
+        return reverse('show_paciente', kwargs={'pk': self.object.id})
+
+
+class PacienteAtendimentoCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/'
+
+    model = Atendimento
+    fields = ['codigo_lif', 'lif']
+    template_name = 'paciente/atendimento_novo.html'
+
+    def form_valid(self, form):
+        form.instance.paciente = Paciente.objects.get(id=self.kwargs['pk'])
+        form.save()
+        return super(PacienteAtendimentoCreateView, self).form_valid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['paciente'] = Paciente.objects.get(id=self.kwargs['pk'])
+        return context
+
+    def get_success_url(self):
+        return reverse('update_atendimento', kwargs={'pk': self.object.id})
+
+
+class PacienteUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/'
+
+    model = Paciente
+    fields = ['nome_completo', 'cpf', 'rg', 'data_nascimento',
+              'sexo', 'tem_responsavel', 'nome_responsavel', 'rg_responsavel']
+    template_name = 'paciente/editar.html'
+
+    def get_success_url(self):
+        return reverse('show_paciente', kwargs={'pk': self.object.id})
 
 
 class PacienteDeleteView(LoginRequiredMixin, DeleteView):
